368c94ba310fabff46e0b622f156c622222183d6
EverySimon <simon.calver@mixergy.co.uk>
2022-04-30 16:48:18 +0100

    Something

--- js/EndScene.js
+++ js/EndScene.js
@@ -1,26 +1,26 @@
 export default class EndScene extends Phaser.Scene {
 
-	constructor() {
-		super("EndScene");
-	}
+  constructor() {
+    super("EndScene");
+  }
 
-	create() {
-		 var bg = this.add.sprite(0,0,'background_1');
-		 bg.setOrigin(0,0);
+  create() {
+    var bg = this.add.sprite(0, 0, 'background_1');
+    bg.setOrigin(0, 0);
 
-		 var text = 'Game Over'
+    var text = 'Game Over'
 
-		 var startText = this.add.text(100,100, text);
+    var startText = this.add.text(100, 100, text);
 
-		 // Add go back button to title screen
-		var backText = this.add.text(100,500, 'Go Back');
-		backText.setInteractive({ useHandCursor: true });
-		backText.on('pointerdown', () => this.backButton());
-		
-	}
+    // Add go back button to title screen
+    var backText = this.add.text(100, 500, 'Go Back');
+    backText.setInteractive({ useHandCursor: true });
+    backText.on('pointerdown', () => this.backButton());
 
-	backButton() {
-		this.scene.start('TitleScene');
-	}
+  }
+
+  backButton() {
+    this.scene.start('TitleScene');
+  }
 
 }
